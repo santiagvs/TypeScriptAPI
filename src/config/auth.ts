4b64e9644f29520a40d33363085b7bb406e975ba
Santiago <victor.lacsilva0102@gmail.com>
2022-09-21 12:18:12 -0300

    feat: added jwt middleware to secure routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '3564d2648230b8fd059a80e52a199846',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/middlewares/isAuthenticated.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+
+  // Bearer + token djfioajfiajof
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodeToken = verify(token, authConfig.jwt.secret);
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token.');
+  }
+}
